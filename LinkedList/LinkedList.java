6ddab31d498a5e4a0dbe8db76433e7dd3c9eb184
Inayath Ali <inayath5026@gmail.com>
2025-02-10 00:30:29 +0530

    Linked List

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,238 @@
+package LinkedList;
+
+public class LinkedList {
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size = 0;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printLinkedList() {
+        if (head == null) {
+            System.out.println("List is Empty !!");
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null && i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        if (temp == null) {
+            System.out.println("Invalid Index");
+            return;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+        if (newNode.next == null) {
+            tail = newNode;
+        }
+    }
+
+    public int removeFirst() {
+
+        if (head == null) {
+            System.out.println("List is Empty");
+            return Integer.MAX_VALUE;
+        } else if (size == 1) {
+
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+
+        }
+
+        int val = head.data;
+        head = head.next;
+        size--;
+
+        return val;
+
+    }
+
+    public int removeLast() {
+
+        if (size == 0) {
+
+            System.out.println("List is Empty !");
+            return Integer.MAX_VALUE;
+
+        } else if (size == 1) {
+
+            int val = tail.data;
+            tail = head = null;
+            size--;
+            return val;
+
+        }
+        Node temp = head;
+        for (int i = 0; i < size - 2; i++) {
+
+            temp = temp.next;
+
+        }
+
+        int val = tail.data;
+        temp.next = null;
+        tail = temp;
+        size--;
+
+        return val;
+
+    }
+
+    public int Search(int key) {
+
+        if (size == 0) {
+            return -1;
+        }
+
+        Node temp = head;
+
+        int i = 0;
+        while (temp != null) {
+
+            if (temp.data == key) {
+                return i;
+            }
+            i++;
+
+            temp = temp.next;
+
+        }
+
+        return -1;
+    }
+
+    public void reverse() {
+
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while (curr != null) {
+
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+
+        head = prev;
+
+    }
+
+    public void findNandRemove(int n) {
+
+        Node temp = head;
+        int sz = 0;
+
+        while (temp != null) {
+
+            temp = temp.next;
+            sz++;
+
+        }
+
+        if (sz - n == 0) {
+            head = head.next;
+            return;
+        }
+
+        Node prev = head;
+
+        int prevIdx = sz - n;
+
+        int i = 1;
+
+        while (i < prevIdx) {
+
+            prev = prev.next;
+            i++;
+
+        }
+
+        prev.next = prev.next.next;
+        return;
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+
+        ll.add(0, 20);
+        ll.add(2, 25);
+        ll.add(5, 30);
+
+        ll.removeFirst();
+        ll.removeLast();
+
+        // System.out.println(ll.Search(22));
+
+        ll.printLinkedList();
+        // ll.reverse();
+        ll.findNandRemove(2);
+        ll.printLinkedList();
+
+        System.out.println("size : " + LinkedList.size);
+    }
+}
